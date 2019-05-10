52de6acfd8ce0a8abcc1d0976f49877171cf4506
TAMAGOR <pizza.prime.57@gmail.com>
2019-05-10 19:16:09 +0900

    表示させる命令を理解した

---
+++ src/components/Itemlist.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Itemlist
+        <div v-for="item in items" :key="item.name">
+            <div class="item">
+                <div class="name">名前:{{item.name}}</div>
+                <div class="class">{{item.price}}円</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:"Itemlist",
+    data(){
+        return{
+            items:[
+                {name:"りんご",price:100},
+                {name:"たまご",price:160}
+            ]
+        }
+    }
+};
+</script>
+
+<style>
+</style>
